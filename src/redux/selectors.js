7af0aa8f6cb55e1164a98176f57485a7fcef2b5a
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-28 20:29:19 +0300

    Feature/ memoized selectVisibleContacts

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,36 +1,26 @@
-// import { initialContacts } from 'dataBase';
-
-// {
-//   contacts: {
-//     items: [],
-//     isLoading: false,
-//     error: null
-//   },
-//   filter: ""
-// }
+import { createSelector } from '@reduxjs/toolkit';
 
 export const selectContacts = state => state.contacts.items;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const selectFilterValue = state => state.filter;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const filter = selectFilterValue(state);
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilterValue],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
 
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter)
-  );
-};
+// export const selectVisibleContacts = state => {
+//   const contacts = selectContacts(state);
+//   const filter = selectFilterValue(state);
 
-// export const selectTestContacts = state => {
-//   const contacts = getContacts(state);
-
-//   return initialContacts.filter(
-//     ({ id: newId }) =>
-//       !contacts
-//         .reduce((acc, { id: prevId }) => [...acc, prevId], [])
-//         .includes(newId)
+//   const normalizedFilter = filter.toLowerCase();
+//   return contacts.filter(({ name }) =>
+//     name.toLowerCase().includes(normalizedFilter)
 //   );
 // };
